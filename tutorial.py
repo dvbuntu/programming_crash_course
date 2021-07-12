3a26df458794adb6a1e45b9a6b4272912dac2a4a
Dan Van Boxel <the.dvb@gmail.com>
2021-07-11 18:36:07 -0700

    Basic tutorial

--- tutorial.py
+++ tutorial.py
@@ -52,6 +52,12 @@ else:
 if False:
     print("Will never show up")
 
+# Read a file
+f = open('sample.txt', 'r') # 'r' for read
+text = f.read()
+f.close() # be sure to clean up
+print(text)
+
 # Lists and dictionaries
 ## Lists store many things in some order
 thing = [1, 2, 'abc']
@@ -80,6 +86,14 @@ print(bar[3])
 ## but you can't set values in a string
 bar[2] = 'x' # Error!
 
+## can convert strings to lists
+baz = list(bar)
+print(baz)
+
+## How long is a list or string?
+z = len(baz)
+print(z)
+
 ## Dictionaries store things indexed by other things!
 info = {'cat':3, 'dog':2, 'wombat':0}
 print(info['cat'])
@@ -101,15 +115,67 @@ for element in [1,2,3]:
 for i in range(10):
     print(i, i**2)
 
-# sum 0-100
+## sum 0-100
 s = 0
 for i in range(101):
     s += i
 print(s)
 
+## make a new list
+s = list()
+for i in range(10,20): # 10, 11, ..., 19
+    s.append(i**2)
+print(s)
 
-* Basic syntax (printing, arithmetic operations, basic types, variables)
-* Conditionals/simple flow control (if...elif...else)
-* Iteration (for loops, maybe while loops, probably some python `range` magic)
-* Functions
-* Useful packages (numpy with basic array ops, maybe scipy, matplotlib)
+# Functions
+## setup a series of commands you can call whenever needed
+def square(x):
+    return x**2
+
+ans = square(4)
+print(ans)
+
+def my_sum(things):
+    ans = 0
+    for i in things:
+        ans += i
+    return ans
+
+ans = my_sum([1,3,5])
+print(ans)
+
+## multiple arguments, with defaults
+def power(base, exp=2):
+    return base**exp
+
+print(power(2)) # use default
+print(power(2, 1)) # by position
+print(power(exp=1, base=2)) # by name, any order
+
+# Plotting
+import matplotlib.pyplot as plt
+plt.ion()
+
+## plot vs index
+plt.plot([1,2,5])
+
+## y vs x
+x = [1,2,5]
+y = [2,4,6]
+plt.plot(x,y)
+
+## bar plot
+plt.bar(x,y)
+
+# Numpy for matrix manipulation and more!
+import numpy as np # can rename module for each access
+
+## works like a list
+a = np.array([1,2,3])
+print(a)
+
+## but list of lists
+b = np.array([[1,2], [3,4]])
+print(b)
+print(b[0])
+print(b[0,1]) # multiple indices!
